9c1e8d5eee8f7a87cc1f6e4cd973ac208df5dfef
Adrian G <92943726+Hifilo@users.noreply.github.com>
2023-06-05 00:22:12 -0400

    updating all files adding new files removed large videos

---
+++ js/main.js
@@ -0,0 +1,45 @@
+// document.querySelector('.button-main').addEventListener('mouseover', function () {
+//     this.querySelector('.progress-bar').style.width = '100%';
+// });
+
+// document.querySelector('.button-main').addEventListener('mouseleave', function () {
+//     this.querySelector('.progress-bar').style.width = '0';
+// });
+
+/*
+	Ripple effect
+	--------------------
+	Draws a background radial gradient on the clicked element around the event mouse coordinates,
+	then it uses a RAF loop to expand the gradients radius until it reaches 100%
+
+	improvements:
+	- get background color - (convert to hsl ?) - use that as a base with the highlight being lighter or darker
+*/
+
+const ripple = (el) => {
+    let d = el.dataset.ripple.split('-'),
+        s = '#f5f5fa',
+        e = 'rgba(3,149,229,1)';
+    d[2] = Number(d[2]) + 4;
+    el.dataset.ripple = d.join('-');
+    el.style.background = `radial-gradient(circle at ${d[0]}px ${d[1]}px, ${s} 0%, ${s} ${d[2]}%, ${e} ${d[2] + 0.1}%)`;
+
+    window.requestAnimationFrame(() => {
+        if (el.dataset.ripple && d[2] < 100) ripple(el);
+    });
+};
+
+const start = (ev) => {
+    ev.target.dataset.ripple = `${ev.offsetX}-${ev.offsetY}-0`;
+    ripple(ev.target);
+};
+
+const stop = (ev) => {
+    let el = document.querySelector('[data-ripple]');
+    delete el.dataset.ripple;
+    el.style.background = 'none';
+};
+
+// Events
+document.querySelectorAll('.button-main').forEach((el) => el.addEventListener('mousedown', start));
+document.addEventListener('mouseup', stop);
--- js/plugins.js
+++ js/plugins.js
@@ -1,24 +1,129 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-plusplus */
+/* eslint-disable no-multi-assign */
 // Avoid `console` errors in browsers that lack a console.
-(function() {
-  var method;
-  var noop = function () {};
-  var methods = [
-    'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-    'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-    'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-    'timeline', 'timelineEnd', 'timeStamp', 'trace', 'warn'
-  ];
-  var length = methods.length;
-  var console = (window.console = window.console || {});
+// eslint-disable-next-line no-undef
+// let grid = new Muuri('.grid');
+// eslint-disable-next-line no-undef
 
-  while (length--) {
-    method = methods[length];
+// let bodyGrid = new Muuri('.bodygrid', {
+//     layout: function (grid, layoutId, items, width, height, callback) {
+//         let layout = {
+//             id: layoutId,
+//             items: items,
+//             slots: [],
+//             styles: {},
+//         };
+//         // Calculate the slots asynchronously. Note that the timeout is here only
+//         // as an example and does not help at all in the calculations. You should
+//         // offload the calculations to web workers if you want real benefits.
+//         // Also note that doing asynchronous layout is completely optional and you
+//         // can call the callback function synchronously also.
+//         let timerId = window.setTimeout(function () {
+//             let item;
+//             let m;
+//             let x = 0;
+//             let y = 0;
+//             let w = 0;
+//             let h = 0;
 
-    // Only stub undefined methods.
-    if (!console[method]) {
-      console[method] = noop;
+//             for (let i = 0; i < items.length; i++) {
+//                 item = items[i];
+//                 x += w;
+//                 y += h;
+//                 m = item.getMargin();
+//                 w = item.getWidth() + m.left + m.right;
+//                 h = item.getHeight() + m.top + m.bottom;
+//                 layout.slots.push(x, y);
+//             }
+
+//             w += x;
+//             h += y;
+//             // Set the CSS styles that should be applied to the grid element.
+//             layout.styles.width = `${w}px`;
+//             layout.styles.height = `${h}px`;
+//             // layout.styles.width = `100vw`;
+//             // layout.styles.height = `100vh`;
+//             // When the layout is fully computed let's call the callback function and
+//             // provide the layout object as it's argument.
+//             callback(layout);
+//         }, 200);
+
+//         // If you are doing an async layout you _can_ (if you want to) return a
+//         // function that cancels this specific layout calculations if it's still
+//         // processing/queueing when the next layout is requested.
+//         return function () {
+//             window.clearTimeout(timerId);
+//         };
+//     },
+// });
+// let baseGrid = new Muuri('.basegrid');
+// let header = new Muuri('.header');
+
+(function () {
+    let bodyGrid = new Muuri('.bodygrid', {
+        layout: {
+            fillGaps: false,
+            horizontal: false,
+            alignRight: false,
+            alignBottom: false,
+            rounding: true,
+        },
+        layoutOnResize: 150,
+        layoutOnInit: true,
+        layoutDuration: 300,
+        layoutEasing: 'ease',
+        dragEnabled: true,
+        showDuration: 600,
+        showEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+    });
+
+    [].slice.call(document.querySelectorAll('.item')).forEach(function (elem) {
+        elem.addEventListener('click', function (e) {
+            e.preventDefault();
+        });
+    });
+
+    var method;
+    var noop = function () {};
+    var methods = [
+        'assert',
+        'clear',
+        'count',
+        'debug',
+        'dir',
+        'dirxml',
+        'error',
+        'exception',
+        'group',
+        'groupCollapsed',
+        'groupEnd',
+        'info',
+        'log',
+        'markTimeline',
+        'profile',
+        'profileEnd',
+        'table',
+        'time',
+        'timeEnd',
+        'timeline',
+        'timelineEnd',
+        'timeStamp',
+        'trace',
+        'warn',
+    ];
+    var length = methods.length;
+    var console = (window.console = window.console || {});
+
+    while (length--) {
+        method = methods[length];
+
+        // Only stub undefined methods.
+        if (!console[method]) {
+            console[method] = noop;
+        }
     }
-  }
-}());
+})();
 
 // Place any jQuery/helper plugins in here.
