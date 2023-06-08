157f813d0e401fa19135f44dd9347270d45c6d88
Adrian G <92943726+Hifilo@users.noreply.github.com>
2023-06-08 02:41:31 -0400

    portfolio is at a functional state
    incorporated muuri for dynamic grid layouts
    
    artwork added
    
    upload static images of UX related work
        incorporate into the design & layout

--- js/main.js
+++ js/main.js
@@ -1,45 +1,72 @@
-// document.querySelector('.button-main').addEventListener('mouseover', function () {
-//     this.querySelector('.progress-bar').style.width = '100%';
-// });
+// // document.querySelector('.button-main').addEventListener('mouseover', function () {
+// //     this.querySelector('.progress-bar').style.width = '100%';
+// // });
 
-// document.querySelector('.button-main').addEventListener('mouseleave', function () {
-//     this.querySelector('.progress-bar').style.width = '0';
-// });
+// // document.querySelector('.button-main').addEventListener('mouseleave', function () {
+// //     this.querySelector('.progress-bar').style.width = '0';
+// // });
 
-/*
-	Ripple effect
-	--------------------
-	Draws a background radial gradient on the clicked element around the event mouse coordinates,
-	then it uses a RAF loop to expand the gradients radius until it reaches 100%
+// /*
+// 	Ripple effect
+// 	--------------------
+// 	Draws a background radial gradient on the clicked element around the event mouse coordinates,
+// 	then it uses a RAF loop to expand the gradients radius until it reaches 100%
 
-	improvements:
-	- get background color - (convert to hsl ?) - use that as a base with the highlight being lighter or darker
-*/
+// 	improvements:
+// 	- get background color - (convert to hsl ?) - use that as a base with the highlight being lighter or darker
+// */
 
-const ripple = (el) => {
-    let d = el.dataset.ripple.split('-'),
-        s = '#f5f5fa',
-        e = 'rgba(3,149,229,1)';
-    d[2] = Number(d[2]) + 4;
-    el.dataset.ripple = d.join('-');
-    el.style.background = `radial-gradient(circle at ${d[0]}px ${d[1]}px, ${s} 0%, ${s} ${d[2]}%, ${e} ${d[2] + 0.1}%)`;
+// const ripple = (el) => {
+//     let d = el.dataset.ripple.split('-'),
+//         s = '#f5f5fa',
+//         e = 'rgba(3,149,229,1)';
+//     d[2] = Number(d[2]) + 4;
+//     el.dataset.ripple = d.join('-');
+//     el.style.background = `radial-gradient(circle at ${d[0]}px ${d[1]}px, ${s} 0%, ${s} ${d[2]}%, ${e} ${d[2] + 0.1}%)`;
 
-    window.requestAnimationFrame(() => {
-        if (el.dataset.ripple && d[2] < 100) ripple(el);
-    });
-};
+//     window.requestAnimationFrame(() => {
+//         if (el.dataset.ripple && d[2] < 100) ripple(el);
+//     });
+// };
 
-const start = (ev) => {
-    ev.target.dataset.ripple = `${ev.offsetX}-${ev.offsetY}-0`;
-    ripple(ev.target);
-};
+// const start = (ev) => {
+//     ev.target.dataset.ripple = `${ev.offsetX}-${ev.offsetY}-0`;
+//     ripple(ev.target);
+// };
 
-const stop = (ev) => {
-    let el = document.querySelector('[data-ripple]');
-    delete el.dataset.ripple;
-    el.style.background = 'none';
-};
+// const stop = (ev) => {
+//     let el = document.querySelector('[data-ripple]');
+//     delete el.dataset.ripple;
+//     el.style.background = 'none';
+// };
 
 // Events
-document.querySelectorAll('.button-main').forEach((el) => el.addEventListener('mousedown', start));
-document.addEventListener('mouseup', stop);
+// document.querySelectorAll('.button-main').forEach((el) => el.addEventListener('mousedown', start));
+// document.addEventListener('mouseup', stop);
+
+function actionToggle() {
+    const action = document.querySelector('.action');
+    const span = action.querySelector('span');
+    const ul = action.querySelector('ul');
+
+    function toggleActive() {
+        action.classList.toggle('active');
+    }
+
+    span.addEventListener('click', function (e) {
+        e.stopPropagation();
+        toggleActive();
+    });
+
+    document.addEventListener('click', function (e) {
+        const { target } = e;
+        const isClickedInsideAction = action.contains(target);
+        const isClickedInsideUl = ul && ul.contains(target);
+
+        if (!isClickedInsideAction || (isClickedInsideUl && target === ul)) {
+            toggleActive();
+        }
+    });
+}
+
+actionToggle();
