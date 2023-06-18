fc4d313f8070e612d9e5f81110d07a6a4b14326d
Adrian G <92943726+Hifilo@users.noreply.github.com>
2023-06-17 22:51:32 -0400

    gallery functioning correctly *knock on wood*

--- js/plugins.js
+++ js/plugins.js
@@ -1,23 +1,4 @@
 (function () {
-    let bodyGrid = new Muuri('.bodygrid', {
-        layout: {
-            fillGaps: true,
-            horizontal: false,
-            alignRight: false,
-            alignBottom: false,
-            rounding: true,
-            Width: '100vw',
-            Height: '100vh',
-        },
-        layoutOnResize: 150,
-        layoutOnInit: true,
-        layoutDuration: 300,
-        layoutEasing: 'ease',
-        dragEnabled: true,
-        showDuration: 600,
-        showEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-    });
-
     let method;
     let noop = function () {};
     let methods = [
@@ -60,3 +41,25 @@
 })();
 
 // Place any jQuery/helper plugins in here.
+let bodyGrid = new Muuri('.bodygrid', {
+    layout: {
+        fillGaps: true,
+        horizontal: false,
+        alignRight: false,
+        alignBottom: false,
+        rounding: false,
+    },
+    styles: {
+        // width: '800px',
+        // height: '1200px',
+    },
+    layoutOnResize: 150,
+    layoutOnInit: true,
+    layoutDuration: 300,
+    layoutEasing: 'ease',
+    dragEnabled: true,
+    showDuration: 600,
+    showEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+});
+
+bodyGrid.refreshItems(true);
