3314d5301ff7141c3b775e80e53f7bce0bf26dba
Adrian G <92943726+Hifilo@users.noreply.github.com>
2023-07-08 17:01:36 -0400

    updated files

--- js/plugins.js
+++ js/plugins.js
@@ -41,7 +41,7 @@
 })();
 
 // Place any jQuery/helper plugins in here.
-let bodyGrid = new Muuri('.bodygrid', {
+let bodyGrid = new Muuri('.body-grid', {
     layout: {
         fillGaps: true,
         horizontal: false,
@@ -61,27 +61,27 @@ let bodyGrid = new Muuri('.bodygrid', {
     showDuration: 600,
     showEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
 });
+
+// let galleryGrid = new Muuri('.gallery-grid', {
+//     layout: {
+//         fillGaps: true,
+//         horizontal: false,
+//         alignRight: false,
+//         alignBottom: false,
+//         rounding: false,
+//     },
+//     styles: {
+//         // width: '800px',
+//         // height: '1200px',
+//     },
+//     layoutOnResize: 150,
+//     layoutOnInit: true,
+//     layoutDuration: 300,
+//     layoutEasing: 'ease',
+//     dragEnabled: false,
+//     showDuration: 600,
+//     showEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+// });
 
 bodyGrid.refreshItems(true);
-let galleryGrid = new Muuri('.gallery-grid', {
-    layout: {
-        fillGaps: true,
-        horizontal: false,
-        alignRight: false,
-        alignBottom: false,
-        rounding: false,
-    },
-    styles: {
-        // width: '800px',
-        // height: '1200px',
-    },
-    layoutOnResize: 150,
-    layoutOnInit: true,
-    layoutDuration: 300,
-    layoutEasing: 'ease',
-    dragEnabled: true,
-    showDuration: 600,
-    showEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
-});
-
-galleryGrid.refreshItems(true);
+// galleryGrid.refreshItems(true);
